180cb6eae39e3a614228871bbe4b22bd2305192e
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-07-27 12:22:44 -0300

    adicionando footer-navigation para celular

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -23,3 +23,15 @@ function proximaImg(){
 }
 
 
+const lista = document.querySelectorAll('.lista')
+
+function ativaLink(){
+    for(let i of lista){
+        i.classList.remove('ativo')
+    }
+    this.classList.add('ativo')
+}
+
+for(let i of lista){
+    i.addEventListener('click', ativaLink)
+}
