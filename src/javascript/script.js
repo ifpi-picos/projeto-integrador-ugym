bef8586ec361ad1cf6d514c0d850e81092b4a7d1
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-07-28 23:44:12 -0300

    Ajustes em toda a responsividade (estou feliz)

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,27 +3,8 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
+//downbar
 
-var radio = document.querySelector('.manual-btn')
-var cont = 1
-
-document.getElementById('radio1').checked = true
-
-setInterval(() => {
-    proximaImg()
-}, 5000)
-//loopimage
-function proximaImg(){
-    cont++
-
-    if(cont > 3){
-        cont = 1 
-    }
-
-    document.getElementById('radio'+cont).checked = true
-}
-
-//downnav
 const lista = document.querySelectorAll('.lista')
 
 function ativaLink(){
@@ -35,4 +16,24 @@ function ativaLink(){
 
 for(let i of lista){
     i.addEventListener('click', ativaLink)
+}
+
+//slider
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
 }
