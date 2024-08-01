c3fb181a684c8f183f09f0c3de3e6c57de59be8c
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-08-01 11:00:04 -0300

    seccionando o javascript

--- src/javascript/scriptnav.js
+++ src/javascript/scriptnav.js
@@ -17,26 +17,3 @@ function ativaLink(){
 for(let i of lista){
     i.addEventListener('click', ativaLink)
 }
-
-//slider
-var radio = document.querySelector('.manual-btn')
-var cont = 1
-
-document.getElementById('radio1').checked = true
-
-setInterval(() => {
-    proximaImg()
-}, 5000)
-
-function proximaImg(){
-    cont++
-
-    if(cont > 3){
-        cont = 1 
-    }
-
-    document.getElementById('radio'+cont).checked = true
-}
-
-
-
