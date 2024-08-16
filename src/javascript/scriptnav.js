7b79b41b993891b4e620461469fb7d8242551dc3
João Pedro de Carvalho Sales <160866462+joaopedroxcs@users.noreply.github.com>
2024-08-16 11:14:06 -0300

    Add files via upload

---
+++ src/javascript/scriptnav.js
@@ -0,0 +1,26 @@
+//sidebar
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+});
+
+//downbar
+
+const lista = document.querySelectorAll('.lista')
+
+function ativaLink(){
+    for(let i of lista){
+        i.classList.remove('ativo')
+    }
+    this.classList.add('ativo')
+}
+
+for(let i of lista){
+    i.addEventListener('click', ativaLink)
+}
+
+function logout(){
+const logout = document.querySelector('#logout')
+localStorage.removeItem('token')
+window.location.href="https://ifpi-picos.github.io/projeto-integrador-ugym/crud.html"
+
+}
